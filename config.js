373b4918f6bfa1b89f570b56c8cea50903548c20
irunnaubachukwu6-pixel <irunnaubachukwu6@gmail.com>
2025-10-15 09:12:25 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+  ownerName: "Caleb 🐉",
+  ownerNumber: "2347079799769",
+  botName: "CALEBS MEGABOT X 🐉",
+  prefix: ".",
+  mode: "public", // public | private
+  status: "🔥 Professional | 400+ Commands | Built by Calebdcreator",
+};
